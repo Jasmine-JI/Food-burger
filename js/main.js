638db0340a6a0c5dff02ca0121fffd54eab7d9ae
JiaChun0216 <jasmine204635@gmail.com>
2022-10-23 00:43:54 +0800

    fix css/html

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 // 卡片光影
 $(function() {
-
     $('.thumbnail').each(function(i, obj){
        var $thumbnail = $(this);
        console.log('div' + i + ':' + $(this)); 
@@ -69,3 +68,21 @@ $(function() {
        });
     });
  });
+
+
+// restaurant_info 個平台評分與評論
+let circularProgress = document.querySelector(".circular-progress"),
+progressValue = document.querySelector(".progress-value");
+
+let progressStartValue = 0,    
+progressEndValue = 90,    
+speed = 100;
+
+let progress = setInterval(() => {
+   progressStartValue++;
+   progressValue.textContent = `${progressStartValue}%`
+   circularProgress.style.background = `conic-gradient(#7d2ae8 ${progressStartValue * 3.6}deg, #ededed 0deg)`
+   if(progressStartValue == progressEndValue){
+      clearInterval(progress);
+   }    
+}, speed);

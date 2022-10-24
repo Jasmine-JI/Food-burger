0a946f5f725ab72e148d7d5b255a3ae32f7cf1fd
JiaChun0216 <jasmine204635@gmail.com>
2022-10-25 07:28:41 +0800

    fix html/css/js

--- js/main.js
+++ js/main.js
@@ -70,19 +70,81 @@ $(function() {
  });
 
 
-// restaurant_info 個平台評分與評論
-let circularProgress = document.querySelector(".circular-progress"),
-progressValue = document.querySelector(".progress-value");
+// restaurant_info 各平台評分與評論
 
-let progressStartValue = 0,    
-progressEndValue = 90,    
-speed = 100;
+window.addEventListener("scroll",function(){
+   
+   if(document.documentElement.scrollTop > 300 && document.documentElement.scrollTop<350){
+      // Food Burger 
 
-let progress = setInterval(() => {
-   progressStartValue++;
-   progressValue.textContent = `${progressStartValue}%`
-   circularProgress.style.background = `conic-gradient(#7d2ae8 ${progressStartValue * 3.6}deg, #ededed 0deg)`
-   if(progressStartValue == progressEndValue){
-      clearInterval(progress);
+let circularProgress1 = document.querySelector(".circular-progress1"),
+progressValue1 = document.querySelector(".progress-value1");
+
+let progressStartValue1 = 0,    
+progressEndValue1 = 4,    
+speed1 = 200;
+
+let progress1 = setInterval(() => {
+   progressStartValue1++;
+   progressValue1.textContent = `${progressStartValue1}`
+   circularProgress1.style.background = `conic-gradient(#ffd558 ${progressStartValue1 * 72}deg, #ededed 0deg)`
+   if(progressStartValue1 == progressEndValue1){
+      clearInterval(progress1);
    }    
-}, speed);
+}, speed1);
+
+// Food Panda
+let circularProgress2 = document.querySelector(".circular-progress2"),
+progressValue2 = document.querySelector(".progress-value2");
+
+let progressStartValue2 = 0,    
+progressEndValue2 = 5,    
+speed2 = 200;
+
+let progress2 = setInterval(() => {
+   progressStartValue2++;
+   progressValue2.textContent = `${progressStartValue2}`
+   circularProgress2.style.background = `conic-gradient(#ffd558 ${progressStartValue2 * 72}deg, #ededed 0deg)`
+   if(progressStartValue2 == progressEndValue2){
+      clearInterval(progress2);
+   }    
+}, speed2);
+
+// Uber Eats
+let circularProgress3 = document.querySelector(".circular-progress3"),
+progressValue3 = document.querySelector(".progress-value3");
+
+let progressStartValue3 = 0,    
+progressEndValue3 = 3,    
+speed3 = 200;
+
+let progress3 = setInterval(() => {
+   progressStartValue3++;
+   progressValue3.textContent = `${progressStartValue3}`
+   circularProgress3.style.background = `conic-gradient(#ffd558 ${progressStartValue3 * 72}deg, #ededed 0deg)`
+   if(progressStartValue3 == progressEndValue3){
+      clearInterval(progress3);
+   }    
+}, speed3);
+
+// Google
+let circularProgress4 = document.querySelector(".circular-progress4"),
+progressValue4 = document.querySelector(".progress-value4");
+
+let progressStartValue4 = 0,    
+progressEndValue4 = 4,    
+speed4 = 200;
+
+let progress4 = setInterval(() => {
+   progressStartValue4++;
+   progressValue4.textContent = `${progressStartValue4}`
+   circularProgress4.style.background = `conic-gradient(#ffd558 ${progressStartValue4 * 72}deg, #ededed 0deg)`
+   if(progressStartValue4 == progressEndValue4){
+      clearInterval(progress4);
+   }    
+}, speed4);
+   }
+});
+
+
+
